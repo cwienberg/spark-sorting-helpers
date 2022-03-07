18d386f04c5ffdabbd0e956021f8633d9d51fd2b
Christopher González Wienberg <christopher.wienberg@gmail.com>
2022-03-06 17:50:30 -0800

    Lint

--- src/main/scala/cwienberg/spark/sorting/SecondarySortKey.scala
+++ src/main/scala/cwienberg/spark/sorting/SecondarySortKey.scala
@@ -8,7 +8,8 @@ private[sorting] case class SecondarySortKey[K: Ordering, V: Ordering](
 }
 
 private[sorting] object SecondarySortKey {
-  implicit def ordering[K: Ordering, V: Ordering]: Ordering[SecondarySortKey[K, V]] = Ordering.by(_.toTuple)
+  implicit def ordering[K: Ordering, V: Ordering]
+    : Ordering[SecondarySortKey[K, V]] = Ordering.by(_.toTuple)
 
   def apply[K: Ordering, V: Ordering](kv: (K, V)): SecondarySortKey[K, V] = {
     new SecondarySortKey(kv._1, kv._2)
