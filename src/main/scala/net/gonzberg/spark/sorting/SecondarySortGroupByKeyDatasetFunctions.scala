6b5e46d14e225ef256b4c529eb0e4a076ee13ac4
Christopher González Wienberg <christopher@gonzberg.net>
2022-06-16 21:17:38 -0700

    Add methods for folding by key in a dataset, using keyed starting values

--- src/main/scala/net/gonzberg/spark/sorting/SecondarySortGroupByKeyDatasetFunctions.scala
+++ src/main/scala/net/gonzberg/spark/sorting/SecondarySortGroupByKeyDatasetFunctions.scala
@@ -1,7 +1,8 @@
 package net.gonzberg.spark.sorting
 
 import net.gonzberg.spark.sorting.util.GroupByKeyIterator
-import net.gonzberg.spark.sorting.util.DatasetRDDLikeFunctions.toDatasetRDDLikeFunctions
+import net.gonzberg.spark.sorting.util.SortHelpers.joinAndFold
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{Column, Dataset, Encoder}
 
@@ -96,10 +97,81 @@ final class SecondarySortGroupByKeyDatasetFunctions[K, V](
     startValue: A,
     op: (A, V) => A,
     orderExprs: Column*
-  )(implicit kSeqVEncoder: Encoder[(K, A)]): Dataset[(K, A)] = {
+  )(implicit kaEncoder: Encoder[(K, A)]): Dataset[(K, A)] = {
     sortedFoldLeftByKey(startValue, op, None, orderExprs)
   }
 
+  /** Groups by key and applies a binary operation using foldLeft
+    * over the values sorted by some implicit ordering
+    * @param startValues an RDD of start values by key
+    * @param op the binary operation for folding
+    * @param numPartitions the number of partitions
+    * @param orderExprs the column(s) to order by within each group
+    * @tparam A the result type of the folding operation
+    * @return Dataset with keys and values, where values are the result
+    *         of applying foldLeft across the sorted values
+    */
+  def sortedFoldLeftByKey[A](
+    startValues: Dataset[(K, A)],
+    op: (A, V) => A,
+    numPartitions: Int,
+    orderExprs: Column*
+  )(implicit
+    keyOrdering: Ordering[K],
+    kaEncoder: Encoder[(K, A)]
+  ): Dataset[(K, A)] = {
+    import dataset.sparkSession.implicits._
+    val repartitionedStartValues = SecondarySortGroupByKeyDatasetFunctions
+      .repartitionAndSort(startValues, Some(numPartitions))
+      .rdd
+    val repartitionedValues = SecondarySortGroupByKeyDatasetFunctions
+      .repartitionAndSort(dataset, Some(numPartitions), orderExprs)
+      .rdd
+    repartitionedStartValues
+      .zipPartitions(
+        repartitionedValues.mapPartitions(
+          new GroupByKeyIterator(_),
+          preservesPartitioning = true
+        ),
+        preservesPartitioning = true
+      )(joinAndFold(op))
+      .toDS()
+  }
+
+  /** Groups by key and applies a binary operation using foldLeft
+    * over the values sorted by some implicit ordering
+    * @param startValues an RDD of start values by key
+    * @param op the binary operation for folding
+    * @param orderExprs the column(s) to order by within each group
+    * @tparam A the result type of the folding operation
+    * @return Dataset with keys and values, where values are the result
+    *         of applying foldLeft across the sorted values
+    */
+  def sortedFoldLeftByKey[A](
+    startValues: Dataset[(K, A)],
+    op: (A, V) => A,
+    orderExprs: Column*
+  )(implicit
+    keyOrdering: Ordering[K],
+    kaEncoder: Encoder[(K, A)]
+  ): Dataset[(K, A)] = {
+    import dataset.sparkSession.implicits._
+    val repartitionedStartValues = SecondarySortGroupByKeyDatasetFunctions
+      .repartitionAndSort(startValues, None)
+      .rdd
+    val repartitionedValues = SecondarySortGroupByKeyDatasetFunctions
+      .repartitionAndSort(dataset, None, orderExprs)
+      .rdd
+    repartitionedStartValues
+      .zipPartitions(
+        repartitionedValues.mapPartitions(
+          new GroupByKeyIterator(_),
+          preservesPartitioning = true
+        ),
+        preservesPartitioning = true
+      )(joinAndFold(op))
+      .toDS()
+  }
 }
 
 object SecondarySortGroupByKeyDatasetFunctions {
@@ -111,8 +183,8 @@ object SecondarySortGroupByKeyDatasetFunctions {
 
   private def repartitionAndSort[K, T](
     dataset: Dataset[(K, T)],
-    numPartitions: Option[Int],
-    orderExprs: Seq[Column]
+    numPartitions: Option[Int] = None,
+    orderExprs: Seq[Column] = Seq.empty[Column]
   ): Dataset[(K, T)] = {
     val keyColumn = col(dataset.columns.head)
     val repartitioned = numPartitions match {
